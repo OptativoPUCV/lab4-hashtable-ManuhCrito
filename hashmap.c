46246420e6a025da0f4f18f54e315bedb6dcd83f
mDiamondDog <126981857+ManuhCrito@users.noreply.github.com>
2024-04-27 01:37:08 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,10 +112,10 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map)
 {
-  
+  return NULL;
 }
 
-Pair * nextMap(HashMap * map) {
-
-    return NULL;
+Pair * nextMap(HashMap * map)
+{
+  return NULL;
 }
